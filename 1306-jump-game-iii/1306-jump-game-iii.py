3eab08f30af445601984d903444f46386a874837
shubh-AM27 <shubhamsinghedu2004@gmail.com>
2026-05-17 23:35:39 +0530

    Time: 35 ms (35.58%), Space: 71.1 MB (6.73%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canReach(self, arr, start):
+
+        visited = set()
+
+        def dfs(i):
+
+            if i < 0 or i >= len(arr) or i in visited:
+                return False
+
+            if arr[i] == 0:
+                return True
+
+            visited.add(i)
+
+            return dfs(i + arr[i]) or dfs(i - arr[i])
+
+        return dfs(start)
