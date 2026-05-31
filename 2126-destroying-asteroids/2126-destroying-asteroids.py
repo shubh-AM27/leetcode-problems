24caa0f865cbb814de8ef758b73d8f4a44a28520
Shubham <shubhamsinghedu2004@gmail.com>
2026-05-31 10:56:44 +0530

    Time: 65 ms (91.36%), Space: 34.1 MB (12.48%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -0,0 +1,13 @@
+class Solution:
+    def asteroidsDestroyed(self, mass, asteroids):
+
+        asteroids.sort()
+
+        for asteroid in asteroids:
+
+            if mass < asteroid:
+                return False
+
+            mass += asteroid
+
+        return True
